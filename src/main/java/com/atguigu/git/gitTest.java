276924db77168df2daa9fa694302a19b54fe9949
weng1424 <1424605786@qq.com>
2022-09-25 21:41:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * @author wzy 2022-09-25  21:39
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
